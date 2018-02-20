eac17b2bf3abd4ab5a5b72a05af3a61cdd7ef6d3
Eduardo Sebastian <esebastian@altiora.es>
2018-02-20 17:43:45 +0100

    Hide payments section

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ PAYMENTS_LOADER = 'VitoriaGasteizPaymentsLoader'
 # ----------------------
 
 # Show Payments section in menu & home options. Default: False.
-SHOW_PAYMENTS = True
+SHOW_PAYMENTS = False
 
 # Configure 'by area' payment breakdown. Default: ['area', 'payee', 'description']
 # PAYMENTS_BREAKDOWN_BY_AREA = ['area', 'payee', 'description']
