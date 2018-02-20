066c7704c588d96fb0f1cb6fdf7e22199863906a
Eduardo Sebastian <esebastian@altiora.es>
2018-02-20 17:43:44 +0100

    Configure budget and payment loaders

--- loaders/vitoriagasteiz_budget_loader.py
+++ loaders/vitoriagasteiz_budget_loader.py
@@ -1,74 +1,125 @@
 # -*- coding: UTF-8 -*-
 from budget_app.models import *
 from budget_app.loaders import SimpleBudgetLoader
-from decimal import *
-import csv
-import os
-import re
+
+
+class BudgetCsvMapper:
+    expenses_mapping = {
+        '2018': {'ic_code': 0, 'fc_code': 1, 'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 7},
+    }
+
+    income_mapping = {
+        '2018': {'full_ec_code': 0, 'description': 1, 'forecast_amount': 2, 'actual_amount': 5},
+    }
+
+    default = '2018'
+
+    def __init__(self, year, is_expense):
+        column_mapping = BudgetCsvMapper.income_mapping
+
+        if is_expense:
+            column_mapping = BudgetCsvMapper.expenses_mapping
+
+        mapping = column_mapping.get(year)
+
+        if not mapping:
+            mapping = column_mapping.get(BudgetCsvMapper.default)
+
+        self.ic_code = mapping.get('ic_code')
+        self.fc_code = mapping.get('fc_code')
+        self.full_ec_code = mapping.get('full_ec_code')
+        self.description = mapping.get('description')
+        self.forecast_amount = mapping.get('forecast_amount')
+        self.actual_amount = mapping.get('actual_amount')
+
 
 class VitoriaGasteizBudgetLoader(SimpleBudgetLoader):
+    # Programme codes have changed in 2015, due to new laws. Since the
+    # application expects a code-programme mapping to be constant over time, we
+    # are forced to amend budget data prior to 2015.
+    # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
+    programme_mapping = {
+        # old programme: new programme
+    }
 
+    # make year data available in the class and call super
+    def load(self, entity, year, path, status):
+        self.year = year
+        SimpleBudgetLoader.load(self, entity, year, path, status)
+
+    # Parse an input line into fields
     def parse_item(self, filename, line):
-        # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
-        # mapping to be constant over time, we are forced to amend budget data prior to 2015.
-        # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
-        programme_mapping = {
-            # old programme: new programme
-            '1340': '1350',     # Protección Civil
-            '1350': '1360',     # Extinción de incendios
-            '1550': '1532',     # Vías públicas
-            '1620': '1621',     # Recogida de residuos
-            '3130': '3110',     # Protección de la salud
-            '3210': '3219',     # Educación preescolar y primaria
-            '3220': '3229',     # Enseñanza secundaria
-            '3230': '3239',     # Promoción educativa
-            '3240': '3260',     # Servicios complementarios de educación
-            '3340': '3341',     # Promoción cultural
-            '4410': '4411',     # Promoción, mantenimiento y desarrollo del transporte
-            '4940': '4911',     # URBAN- Arona 2007-2013
-        }
-        programme_mapping_2015 = {
-            '3340': '3341',     # Promoción cultural
-        }
+        # Type of data
+        is_expense = (filename.find('gastos.csv') != -1)
+        is_actual = (filename.find('/ejecucion_') != -1)
 
-        # Some dirty lines in input data
-        if line[0]=='':
-            return None
+        # Mapper
+        mapper = BudgetCsvMapper(self.year, is_expense)
 
-        is_expense = (filename.find('gastos.csv')!=-1)
-        is_actual = (filename.find('/ejecucion_')!=-1)
+        # Expenses
         if is_expense:
-            # The input data combines functional and economic codes in a very unusual way
-            match = re.search('^ *(\d+) +(\d+) *', line[0])
-            # We got 3- or 4- digit functional codes as input, so add a trailing zero
-            fc_code = match.group(1).ljust(4, '0')
-            ec_code = match.group(2)
+            # Institutional code
+            # We got 4 or 6 digit institutional codes as input, so we normalize
+            # them at 6 and add trailing zeroes when required
+            ic_code = '000' #line[mapper.ic_code].ljust(6, '0')
 
-            # For years before 2015 we check whether we need to amend the programme code
-            year = re.search('municipio/(\d+)/', filename).group(1)
-            if int(year) < 2015:
-                fc_code = programme_mapping.get(fc_code, fc_code)
-            elif int(year) == 2015:
-                fc_code = programme_mapping_2015.get(fc_code, fc_code)
+            # Functional code
+            fc_code = line[mapper.fc_code]
 
-            return {
-                'is_expense': True,
-                'is_actual': is_actual,
-                'fc_code': fc_code,
-                'ec_code': ec_code[:-2],        # First three digits (everything but last two)
-                'ic_code': '000',
-                'item_number': ec_code[-2:],    # Last two digits
-                'description': line[1],
-                'amount': self._parse_amount(line[5 if is_actual else 2])
-            }
+            # For pre 2015 budgets we may need to amend the programme code
+            if int(self.year) < 2015:
+                fc_code = VitoriaGasteizBudgetLoader.programme_mapping.get(fc_code, fc_code)
 
+            # Economic code
+            full_ec_code = line[mapper.full_ec_code]
+
+            # Concepts are the first 3 digits from the economic codes
+            ec_code = full_ec_code[:3]
+
+            # Item numbers are the last 2 digits from the economic codes
+            # (digits 4 and 5)
+            item_number = full_ec_code[-2:]
+
+            # Description
+            description = line[mapper.description]
+
+            # Parse amount
+            amount = line[mapper.actual_amount if is_actual else mapper.forecast_amount]
+            amount = self._parse_amount(amount)
+
+        # Income
         else:
-            return {
-                'is_expense': False,
-                'is_actual': is_actual,
-                'ec_code': line[0][:-2],        # First three digits
-                'ic_code': '000',               # All income goes to the root node
-                'item_number': line[0][-2:],    # Fourth and fifth digit; careful, there's trailing dirt
-                'description': line[1],
-                'amount': self._parse_amount(line[5 if is_actual else 2])
-            }
+            # Institutional code (all income goes to the root node)
+            ic_code = '000'
+
+            # Functional code
+            # We don't have functional codes in income
+            fc_code = None
+
+            # Economic code
+            full_ec_code = line[mapper.full_ec_code]
+
+            # Concepts are the firts 3 digits from the economic codes
+            ec_code = full_ec_code[:3]
+
+            # Item numbers are the last 2 digits from the economic codes
+            # (digits 4 and 5)
+            item_number = full_ec_code[-2:]
+
+            # Description
+            description = line[mapper.description]
+
+            # Parse amount
+            amount = line[mapper.actual_amount if is_actual else mapper.forecast_amount]
+            amount = self._parse_amount(amount)
+
+        return {
+            'is_expense': is_expense,
+            'is_actual': is_actual,
+            'fc_code': fc_code,
+            'ec_code': ec_code,
+            'ic_code': ic_code,
+            'item_number': item_number,
+            'description': description,
+            'amount': amount
+        }
--- loaders/vitoriagasteiz_payments_loader.py
+++ loaders/vitoriagasteiz_payments_loader.py
@@ -1,27 +1,94 @@
 # -*- coding: UTF-8 -*-
-import datetime
-
 from budget_app.loaders import PaymentsLoader
 from budget_app.models import Budget
 
+import datetime
+
+
+class PaymentsCsvMapper:
+    column_mapping = {
+        '2018': {'fc_code': 9, 'date': 4, 'payee': 12, 'description': 13, 'amount': 5},
+    }
+
+    default = '2018'
+
+    def __init__(self, year):
+        column_mapping = PaymentsCsvMapper.column_mapping
+        mapping = column_mapping.get(year)
+
+        if not mapping:
+            mapping = column_mapping.get(PaymentsCsvMapper.default)
+
+        self.fc_code = mapping.get('fc_code')
+        self.date = mapping.get('date')
+        self.payee = mapping.get('payee')
+        self.description = mapping.get('description')
+        self.amount = mapping.get('amount')
+
+
 class VitoriaGasteizPaymentsLoader(PaymentsLoader):
+    # make year data available in the class and call super
+    def load(self, entity, year, path):
+        self.year = year
+        PaymentsLoader.load(self, entity, year, path)
 
     # Parse an input line into fields
     def parse_item(self, budget, line):
+        # Mapper
+        mapper = PaymentsCsvMapper(self.year)
 
-        policy_id = line[1].strip()[:2] # First two digits of the programme make the policy id
-        # But what we want as area is the policy description
+        # For the functional code We got decimal values as input, so we
+        # normalize them at 4- and add leading zeroes when required
+        fc_code = line[mapper.fc_code].split('.')[0].rjust(4, '0')
+
+        # We ignore rows with incomplete data
+        if fc_code == '0000':
+            return
+
+        # first two digits of the functional code make the policy id
+        policy_id = fc_code[:2]
+
+        # but what we want as area is the policy description
         policy = Budget.objects.get_all_descriptions(budget.entity)['functional'][policy_id]
+
+        # We have got dates as numeric values, per XLS convention: integer part
+        # stores the number of days since the epoch (Jan 1st 1900 in our case)
+        # and the fractional part stores the percentage of the day
+        date = line[mapper.date].strip()
+
+        # serial number that represents the number of elapsed days since January 1, 1900
+        days = int(float(date))
+
+        # actual date taking into account the Excel 1900 leap year bug
+        date = datetime.datetime(1899, 12, 30) + datetime.timedelta(days, 0, 0, 0)
+        date = date.strftime("%Y-%m-%d")
+
+        # Payee data
+        payee = line[mapper.payee].strip()
+
+        # Some rows doesn't include payee data, so we asign an arbitrary value
+        if not payee:
+            payee = "OTROS"
+
+        # We haven't got any anonymized entries
+        anonymized = False
+
+        # Description
+        description = line[mapper.description].strip()
+
+        # Parse amount
+        amount = line[mapper.amount].strip()
+        amount = self._read_english_number(amount)
 
         return {
             'area': policy,
             'programme': None,
             'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
             'ec_code': None,
-            'date': line[3].strip(),
+            'date': date,
             'contract_type': None,
-            'payee': self._titlecase(line[5].strip()),
-            'anonymized': False,
-            'description': line[6].strip(),
-            'amount': self._read_english_number(line[7])
+            'payee': payee,
+            'anonymized': anonymized,
+            'description': description,
+            'amount': amount
         }
