4c4012299cceb659f9d3ffaaaa35161927846c7f
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2018-03-08 12:43:26 +0100

    Expand table in institutional tab

--- static/javascripts/theme.js
+++ static/javascripts/theme.js
@@ -1,9 +1,26 @@
 // Theme custom js methods
 $(document).ready(function(){
 
-  // Hide institutional tab in income articles 
+  var expandPoliciesTable = function () {
+    setTimeout(function() {
+      $('#myGrid a.toggle.expand').trigger('click');
+    }, 500);
+  }
+  
   var state = getUIState();
+ 
+  // Hide institutional tab in income articles 
   if (state.field == 'income' && state.view == 'economic') {
     $('#tabs a[href="#institutional"]').remove();
   }
+
+  // Expand table for institutional tab
+  if (state.view == 'functional' || state.view == 'economic') {
+    $('#tabs a[href="#institutional"]').click(expandPoliciesTable);
+    $('#year-selection').change(expandPoliciesTable);
+  } else if  (state.view == 'institutional') {
+    expandPoliciesTable();
+    $('#year-selection').change(expandPoliciesTable);
+  }
+
 });
