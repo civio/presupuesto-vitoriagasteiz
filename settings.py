f9ef7931ab2b3926b612e890118a6fa694c472c4
David Cabo <david.cabo@gmail.com>
2023-04-11 01:42:52 +0200

    Remove obsolete Facebook ID civio/presupuesto-management#1257

--- settings.py
+++ settings.py
@@ -87,10 +87,6 @@ LANGUAGES = (
   ('es', 'Castellano'),
 )
 
-# Facebook Aplication ID used in social_sharing temaplate. Default: ''
-# In order to get the ID create an app in https://developers.facebook.com/
-FACEBOOK_ID = '101568380658287'
-
 # Google Analytics ID. Default: ''
 # In order to get the ID create a Google Analytics Acount in https://analytics.google.com/analytics/web/
 ANALYTICS_ID = 'UA-28946840-43'
