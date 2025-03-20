22f033268111a2f8b8e2fbb31978f17c1d1d5ec0
David Cabo <david.cabo@gmail.com>
2025-03-20 22:22:41 +0100

    Use Python 3-compatible imports civio/presupuesto-management#1286

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,2 +1,8 @@
-from vitoriagasteiz_budget_loader import VitoriaGasteizBudgetLoader
-from vitoriagasteiz_payments_loader import VitoriaGasteizPaymentsLoader
+import six
+
+if six.PY2:
+    from vitoriagasteiz_budget_loader import VitoriaGasteizBudgetLoader
+    from vitoriagasteiz_payments_loader import VitoriaGasteizPaymentsLoader
+else:
+    from .vitoriagasteiz_budget_loader import VitoriaGasteizBudgetLoader
+    from .vitoriagasteiz_payments_loader import VitoriaGasteizPaymentsLoader
